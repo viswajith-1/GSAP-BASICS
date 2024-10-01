f1c1ff1435320f5a7ab4c52041fb189fe77a66a8
Viswajith SB <viswajithsaji@gmail.com>
2024-10-01 22:51:22 +0530

    progressing file

--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -1,5 +1,25 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
+  useGSAP(()=>{
+    gsap.to('.stagger-box', {
+      y:200,
+      repeat:-1,
+      yoyo:true,
+      rotation:360,
+      borderRadius:100,
+      //stagger:0.5
+      stagger:{
+        amount:1.5,
+        grid: [2,1],
+        axis:"y",
+        from:'center',
+      }
+
+    })
+  }, [])
 
   return (
     <main>
